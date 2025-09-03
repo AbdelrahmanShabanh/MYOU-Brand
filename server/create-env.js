0f5d3ccd0ee31a2cbcf960cc8939e7c129a05452
AbdelrahmanShabanh <bodysh2019@gmail.com>
2025-09-03 05:21:28 +0300

    build errors

--- server/create-env.js
+++ server/create-env.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const envContent = `MONGODB_URI=mongodb+srv://bodysh2019:1fvYHtlP5PESxtdM@myou.j6sr5u0.mongodb.net/?retryWrites=true&w=majority&appName=Myou
-JWT_SECRET=your-super-secret-jwt-key-change-this-in-production
+JWT_SECRET=${JWT_SECRET}
 PORT=5000
 CLOUDINARY_CLOUD_NAME=dbf5zfdx8
 CLOUDINARY_API_KEY=139946622523681
